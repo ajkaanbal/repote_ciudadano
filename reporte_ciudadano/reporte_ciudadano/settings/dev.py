430657327eba260803125fb554186c13dff286bc
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2015-01-14 01:01:53 -0600

    in the begining, darkness was over the surface of the deep...

---
+++ reporte_ciudadano/reporte_ciudadano/settings/dev.py
@@ -0,0 +1,14 @@
+from .common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# INSTALLED_APPS += ()
